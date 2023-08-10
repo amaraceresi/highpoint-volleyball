99b00f56292d30ed754e613798d7dfee57605947
Amara Ceresi <aceresi95@gmail.com>
2023-08-09 21:57:12 -0500

    make mobile responsive

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -12,21 +12,20 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function Navbar() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const { isAuthenticated } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const handleToggleDrawer = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
+  const handleToggleDrawer = () => setDrawerOpen(!drawerOpen);
   const handleLogout = () => {
     AuthService.logout();
     dispatch(logOut());
   };
-
   const links = [
     { to: '/', text: 'HOME' },
     { to: '/contact', text: 'CONTACT' },
@@ -36,7 +35,6 @@ function Navbar() {
     isAuthenticated && { to: '/dashboard', text: 'DASHBOARD' },
     isAuthenticated && { onClick: handleLogout, text: 'LOGOUT' },
   ].filter(Boolean);
-
   const userLinks = [
     !isAuthenticated && { to: '/login', text: 'LOG IN' },
     !isAuthenticated && { to: '/signup', text: 'SIGN UP' },
@@ -46,36 +44,14 @@ function Navbar() {
     <>
       <AppBar position="static" style={{ backgroundColor: '#1a237e', fontFamily: 'Orbitron, sans-serif' }}>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleToggleDrawer}>
-            <MenuIcon />
-          </IconButton>
-          <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <ul style={{ display: 'flex', listStyle: 'none', padding: 0, margin: 0 }}>
-              {links.map((link, index) => (
-                <li key={index} style={{ margin: '0 15px' }}>
-                  <Typography variant="body1">
-                    {link.to ? <Link to={link.to} style={{ color: 'white', textDecoration: 'none' }}>{link.text}</Link> : <Button onClick={link.onClick} style={{ color: 'white' }}>{link.text}</Button>}
-                  </Typography>
-                </li>
-              ))}
-            </ul>
+          {isMobile && <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleToggleDrawer}><MenuIcon /></IconButton>}
+          <div style={{ flexGrow: 1, display: 'flex', justifyContent: isMobile ? 'center' : 'space-between', alignItems: 'center' }}>
+            {!isMobile && <ul style={{ display: 'flex', listStyle: 'none', padding: 0, margin: 0 }}>{links.map((link, index) => <li key={index} style={{ margin: '0 15px' }}><Typography variant="body1">{link.to ? <Link to={link.to} style={{ color: 'white', textDecoration: 'none' }}>{link.text}</Link> : <Button onClick={link.onClick} style={{ color: 'white' }}>{link.text}</Button>}</Typography></li>)}</ul>}
           </div>
-          {userLinks.map((link, index) => (
-            <Button variant="contained" color="primary" key={index} component={Link} to={link.to} style={{ marginLeft: '10px' }}>
-              {link.text}
-            </Button>
-          ))}
+          {!isMobile && userLinks.map((link, index) => <Button variant="contained" color="primary" key={index} component={Link} to={link.to} style={{ marginLeft: '10px' }}>{link.text}</Button>)}
         </Toolbar>
       </AppBar>
-      <Drawer anchor="left" open={drawerOpen} onClose={handleToggleDrawer}>
-        <List>
-          {links.concat(userLinks).map((link, index) => (
-            <ListItem button key={index}>
-              {link.to ? <Link to={link.to} onClick={handleToggleDrawer} style={{ color: 'black', textDecoration: 'none' }}>{link.text}</Link> : <Button onClick={link.onClick}>{link.text}</Button>}
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+      {isMobile && <Drawer anchor="left" open={drawerOpen} onClose={handleToggleDrawer}><List>{links.concat(userLinks).map((link, index) => <ListItem button key={index}>{link.to ? <Link to={link.to} onClick={handleToggleDrawer} style={{ color: 'black', textDecoration: 'none' }}>{link.text}</Link> : <Button onClick={link.onClick}>{link.text}</Button>}</ListItem>)}</List></Drawer>}
     </>
   );
 }
